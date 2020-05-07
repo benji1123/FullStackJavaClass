87e9ac9b0040b301669174d0d6324d352e600935
Ben <benji112358@gmail.com>
2020-05-07 15:16:01 -0400

    first JSP prj

---
+++ JSP/1_helloworld/README.md
@@ -0,0 +1,2 @@
+**run `servlet_one.java in `src/<package>`**
+blank webpage generates; and "bonjour monde" is printed on console
---
+++ JSP/1_helloworld/src/com/jsp_ex1/servlets/servelet_one.java
@@ -0,0 +1,34 @@
+package com.jsp_ex1.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servelet_one
+ */
+@WebServlet("/servelet_one")
+public class servelet_one extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servelet_one() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("bonjour monde");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
